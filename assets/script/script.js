262530bff8aec587c2f1cbe863dc13223c118442
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-06-29 21:52:45 -0700

    add updateSlots function to color each timeslot

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,5 +11,20 @@ function updateTime () {
 
 updateTime();
 setInterval(function() {
-    updateTime()
+    updateTime();
+    updateSlots();
 }, 1000)
+
+function updateSlots() {
+    currentTime = now.format('HH');
+    container = $('#timeblocks').children()
+    for (i=0; i<container.length; i++) {
+        if (parseInt(currentTime) > parseInt(container.eq(i).attr('id'))) {
+            container.eq(i).css('backgroundColor', 'red') //bg color to red
+        } else if (parseInt(currentTime) === parseInt(container.eq(i).attr('id'))) {
+            container.eq(i).css('backgroundColor', 'grey')
+        } else (container.eq(i).css('backgroundColor', 'green'))
+    }
+}
+
+updateSlots()
