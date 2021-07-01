98da23059728fb67002506bd2f3d1f705ebaf374
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-06-30 17:36:34 -0700

    add contenteditable tags to html, add base array for local storage to JS

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,19 +1,20 @@
-var now = moment();
+/**********************************************************************************
+ * Header date and updated clock and giving colors to timeblocks
+ * functions: updateTime, updateSlots
+ */
+var now = moment(); //current date&time
 
-$('#currentDay').text(now.format('[Today is ]MMMM Do, YYYY'));
+$('#currentDay').text(now.format('[Today is ]MMMM Do, YYYY')); //Give subtitle a date
 
-
-function updateTime () {
+function updateTime () { //updating the clock with current timer
     timer = moment().format('hh:mm:ss')
     $('#currentTime').text(timer);
 }
 
-
-updateTime();
-setInterval(function() {
+setInterval(function() { //refreshes the functions and colors in timeslots every .1 second
     updateTime();
     updateSlots();
-}, 1000)
+}, 100)
 
 function updateSlots() {
     currentTime = now.format('HH');
@@ -29,5 +30,55 @@ function updateSlots() {
         } else (container.find("#textDisplay").eq(i).addClass('future'))
     }
 }
+//***************************************************************** */
 
-updateSlots()
+/*******************************************************************
+ * Editing and saving the descriptions.  Also updating the local storage.
+ * Functions: editDescription, saveDescription
+ * 
+ */
+
+//taskDescript(ion) contains an base array of objects that have a key:value of time:(an integer) and tasks:(a string)
+taskDescript= [ 
+    {   'time': 9,
+        'tasks': "No tasks this hour!"
+    },    {
+        'time': 10,
+        'tasks': "No tasks this hour!"
+    },    {
+        'time': 11,
+        'tasks': "No tasks this hour!"
+    },    {
+        'time': 12,
+        'tasks': "No tasks this hour!"
+    },    {
+        'time': 13,
+        'tasks': "No tasks this hour!"
+    },    {
+        'time': 14,
+        'tasks': "No tasks this hour!"
+    },    {
+        'time': 15,
+        'tasks': "No tasks this hour!"
+    },    {
+        'time': 16,
+        'tasks': "No tasks this hour!"
+    },    {
+        'time': 17,
+        'tasks': "No tasks this hour!"
+    },
+]
+
+
+function editDescription (event) {
+    console.log(event);
+}
+
+function saveDescription (event) {
+    console.log(event);
+}
+
+
+//Adding and delagating click events on the page.
+$('.description').on('click', editDescription)
+$("button[type='button']").on('click', saveDescription)
